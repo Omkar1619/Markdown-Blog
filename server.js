ff7f0091f14e168c32936e14cacdbc321fdafaa0
Omkar <9537732360omkar@gmail.com>
2024-01-04 00:04:24 +0530

    uploaded all files

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const app = express()
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+
+mongoose.connect(process.env.MONGO_URL)
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('articles/index', {articles : articles})
+})
+
+app.use('/articles', articleRouter)
+app.listen(process.env.PORT)
